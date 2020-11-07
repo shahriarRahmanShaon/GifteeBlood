aaa66761e7a1c4cd7a1439d3ceb034e3e98685a2
shahriarRahmanShaon <shaon463@gmail.com>
2020-11-08 00:49:26 +0600

    Initial Commit

---
+++ GifteeBlood/GifteeBloodApp.swift
@@ -0,0 +1,17 @@
+//
+//  GifteeBloodApp.swift
+//  GifteeBlood
+//
+//  Created by sergio shaon on 8/11/20.
+//
+
+import SwiftUI
+
+@main
+struct GifteeBloodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
